e52272214c73e834ed94430401df71b2c287e962
Trixtaro <wilmerdavid97@hotmail.com>
2019-05-15 07:00:08 -0500

    Se agrego un menu File con la opcion Salir

--- saccrec/gui/MainWindow.py
+++ saccrec/gui/MainWindow.py
@@ -1,5 +1,29 @@
-from PyQt5.QtWidgets import QMainWindow
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtGui import QIcon
 
 
 class MainWindow(QMainWindow):
-    pass
+
+    def __init__(self):
+    	super().__init__()
+    	self.initUI()
+
+    def hola():
+    	pass
+
+    def initUI(self):
+        exitAct = QAction(QIcon('exit.png'), '&Exit', self)        
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+        
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Simple menu')    
+        self.show()
