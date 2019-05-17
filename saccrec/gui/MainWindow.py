026babfafa6e57da0433b695a1aa0abdd956603d
Trixtaro <wilmerdavid97@hotmail.com>
2019-05-16 23:20:11 -0500

    Se agrego un metodo para creacion de menus y se agrego imagenes a las opciones

--- saccrec/gui/MainWindow.py
+++ saccrec/gui/MainWindow.py
@@ -9,21 +9,27 @@ class MainWindow(QMainWindow):
     	super().__init__()
     	self.initUI()
 
-    def hola():
-    	pass
+    def newMenu(self, nombre):
+        menubar = self.menuBar()
+        menu = menubar.addMenu(nombre)
+        return menu
 
     def initUI(self):
-        exitAct = QAction(QIcon('exit.png'), '&Exit', self)        
+        exitAct = QAction(QIcon('saccrec/gui/images/exit.svg'), '&Exit', self)        
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(qApp.quit)
 
+        aboutUs = QAction(QIcon('saccrec/gui/images/interrogacion.png'), '&About Us',self)
+
         self.statusBar()
 
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
+        fileMenu = self.newMenu('&File')
         fileMenu.addAction(exitAct)
+
+        helpMenu = self.newMenu('&Help')
+        helpMenu.addAction(aboutUs)
         
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Simple menu')    
+        self.setWindowTitle('EyeTracker OpenBCI')
         self.show()
