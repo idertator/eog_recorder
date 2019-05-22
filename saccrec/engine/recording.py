5277c681a1004133248875d3756ba1247b6c7df2
mauroFZ5 <maurozambrano36@hotmail.com>
2019-05-22 09:19:30 -0500

    Se agrego codigo a recording.py

---
+++ saccrec/engine/recording.py
@@ -0,0 +1,99 @@
+import time
+from openbci_interface import Cyton
+import serial
+
+sample_rate = 250
+
+puerto = '/dev/ttyUSB0'
+port = serial.Serial(port=puerto,baudrate=115200,timeout=2)
+
+with Cyton(port) as board:
+    board.set_board_mode('default')
+    board.set_sample_rate(sample_rate)
+    board.start_streaming()
+    while True:
+        sample = board.read_sample()
+        time.sleep(0.95 / sample_rate)
+
+"""Implements ``stream`` command."""
+import sys
+import json
+import time
+import logging
+import argparse
+
+from serial import Serial
+from openbci_interface import Cyton
+
+_LG = logging.getLogger(__name__)
+
+
+def _parse_args(args):
+    parser = argparse.ArgumentParser(
+        description='Stream data from an OpenBCI board.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        '--port', required=True,
+        help='Port to which OpenBCI board is connected.'
+    )
+    parser.add_argument(
+        '--baudrate', '--br', type=int,
+        help='Baudrate.',
+        default=115200,
+    )
+    parser.add_argument(
+        '--timeout', type=int,
+    )
+    parser.add_argument(
+        '--board-type', choices=['cyton', 'ganglion', 'daisy'],
+        default='cyton',
+    )
+    parser.add_argument(
+        '--sample-rate', type=int, default=250,
+        choices=[250, 500, 1000, 2000, 4000, 8000, 16000],
+    )
+    parser.add_argument(
+        '--board-mode', default='default',
+        choices=['default', 'debug', 'analog', 'digital', 'marker'],
+    )
+    parser.add_argument('--debug', action='store_true')
+    return parser.parse_args(args)
+
+
+def main(args):
+    """Entrypoint for ``stream`` command.
+    For the detail of the command, use ``stream --help``.
+    """
+    args = _parse_args(args)
+
+    with Cyton(_get_serial(args)) as board:
+        board.set_board_mode(args.board_mode)
+        board.set_sample_rate(args.sample_rate)
+        board.start_streaming()
+        try:
+            _run(board)
+        except KeyboardInterrupt:
+            pass
+
+
+def _run(board):
+    cycle = 0.85 * board.cycle #board.cycle: tiempo secs en tomar una muestra
+    unit_wait = cycle / 10.0
+    last_acquired = time.monotonic()
+    while True:
+        now = time.monotonic() #monotonic(): devuelve el tiempo secs.mili del sistema
+        if now - last_acquired < cycle:
+            time.sleep(unit_wait) # delay de espera para la proxima muestra
+            continue
+        sample = board.read_sample()
+        last_acquired = now
+        sys.stdout.write(json.dumps(sample))
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+
+
+def _get_serial(args):
+    return Serial(
+        port=args.port, baudrate=args.baudrate, timeout=args.timeout,
+    )
