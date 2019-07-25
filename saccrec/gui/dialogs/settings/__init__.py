e8851a2b8777706826ae3c7a50f0958eb1c84c47
Rodolfo Valentin Becerra Garcia <rodolvbg@gmail.com>
2019-07-25 15:55:55 -0400

    Se añaden los elementos al menu de cofiguracion

---
+++ saccrec/gui/dialogs/settings/__init__.py
@@ -0,0 +1,92 @@
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QDialog, QListWidget, QListView, QStackedWidget, QListWidgetItem, QVBoxLayout, QHBoxLayout, \
+    QDialogButtonBox
+
+from saccrec.core import Settings
+from saccrec.gui.dialogs.settings.openbci_channels import OpenBCIChannelsSettingsPage
+from saccrec.gui.dialogs.settings.openbci import OpenBCISettingsPage
+from saccrec.gui.dialogs.settings.screen import ScreenSettingsPage
+from saccrec.gui.dialogs.settings.stimulus import StimulusSettingsPage
+
+
+class SettingsDialog(QDialog):
+    def __init__(self, settings: Settings, parent=None):
+        super(SettingsDialog, self).__init__(parent=parent)
+        self._settings = settings
+
+        self.contentsWidget = QListWidget()
+        self.contentsWidget.setFlow(QListView.TopToBottom)
+        self.contentsWidget.setViewMode(QListView.IconMode)
+        self.contentsWidget.setMovement(QListView.Static)
+        self.contentsWidget.setFixedWidth(140)
+        self.contentsWidget.setFixedHeight(260)
+        self.contentsWidget.setSpacing(5)
+
+        self.pagesWidget = QStackedWidget()
+        self.pagesWidget.addWidget(OpenBCISettingsPage(self._settings))
+        self.pagesWidget.addWidget(OpenBCIChannelsSettingsPage(self._settings))
+        self.pagesWidget.addWidget(ScreenSettingsPage(self._settings))
+        self.pagesWidget.addWidget(StimulusSettingsPage(self._settings))
+        self.contentsWidget.setCurrentRow(0)
+
+        openbci_button = QListWidgetItem(QIcon(':openbci.png'), 'OpenBCI')
+        openbci_button.setSizeHint(QSize(125, 50))
+        self.contentsWidget.addItem(openbci_button)
+        openbci_button.setTextAlignment(Qt.AlignHCenter)
+        openbci_button.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        channel_button = QListWidgetItem(QIcon(':channels.svg'), 'Canales')
+        channel_button.setSizeHint(QSize(125, 50))
+        self.contentsWidget.addItem(channel_button)
+        channel_button.setTextAlignment(Qt.AlignHCenter)
+        channel_button.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        screen_button = QListWidgetItem(QIcon(':screen.svg'), 'Pantalla de estimulo')
+        screen_button.setSizeHint(QSize(125, 50))
+        self.contentsWidget.addItem(screen_button)
+        screen_button.setTextAlignment(Qt.AlignHCenter)
+        screen_button.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        stimulus_button = QListWidgetItem(QIcon(':stimuli.svg'), 'Estimulo')
+        stimulus_button.setSizeHint(QSize(125, 50))
+        self.contentsWidget.addItem(stimulus_button)
+        stimulus_button.setTextAlignment(Qt.AlignHCenter)
+        stimulus_button.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        self.contentsWidget.currentItemChanged.connect(self.change_page)
+
+        layout = QVBoxLayout()
+        horizontal_layout = QHBoxLayout()
+        horizontal_layout.addWidget(self.contentsWidget)
+        horizontal_layout.addWidget(self.pagesWidget, 1)
+
+        dialog_buttons = QDialogButtonBox()
+        dialog_buttons.addButton('Aplicar', QDialogButtonBox.AcceptRole)
+        dialog_buttons.addButton('Cancelar', QDialogButtonBox.RejectRole)
+        dialog_buttons.accepted.connect(self.on_accepted)
+        dialog_buttons.rejected.connect(self.on_rejected)
+        layout.addLayout(horizontal_layout)
+        layout.addWidget(dialog_buttons)
+
+        self.setLayout(layout)
+
+    def change_page(self, current, previous):
+        if not current:
+            current = previous
+
+        self.pagesWidget.setCurrentIndex(self.contentsWidget.row(current))
+        self.setWindowTitle(self.pagesWidget.currentWidget().title)
+
+    def on_accepted(self):
+        for i in range(self.pagesWidget.count()):
+            self.pagesWidget.widget(i).save()
+        self.accept()
+
+    def on_rejected(self):
+        self.reject()
+
+    def open(self):
+        for i in range(self.pagesWidget.count()):
+            self.pagesWidget.widget(i).load_settings()
+        super(SettingsDialog, self).open()
---
+++ saccrec/gui/dialogs/settings/openbci_channels.py
@@ -0,0 +1,71 @@
+from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import QWidget, QFormLayout, QCheckBox, QSpinBox, QVBoxLayout, QHBoxLayout
+
+from saccrec.consts import SETTINGS_OPENBCI_DEFAULT_GAIN, SETTINGS_OPENBCI_DEFAULT_CHANNEL_NUMBER
+from saccrec.core import Settings
+
+class OpenBCIChannelWidget(QWidget):
+    def __init__(self, channel_number: int, parent=None):
+        super(OpenBCIChannelWidget, self).__init__(parent)
+        self._channel_number = channel_number
+        self._settings = QSettings('SaccRec', 'SaccRec', None)
+
+        layout = QFormLayout()
+
+        self._openbci_channel_activated_check = QCheckBox()
+        self._openbci_channel_activated_check.setChecked(True)
+        self._openbci_channel_activated_check.stateChanged.connect(self.on_activated_change)
+        layout.addRow(f'Canal {self._channel_number + 1}', self._openbci_channel_activated_check)
+
+        self._openbci_channel_gain_edit = QSpinBox()
+        self._openbci_channel_gain_edit.setFixedWidth(40)
+        layout.addRow('Ganancia', self._openbci_channel_gain_edit)
+
+        self.setLayout(layout)
+
+    def on_activated_change(self):
+        self._openbci_channel_gain_edit.setEnabled(self._openbci_channel_activated_check.checkState())
+
+    def load_settings(self):
+        self._openbci_channel_activated_check.setChecked(
+            int(self._settings.value(f'OpenBCIChannelActivated{self._channel_number}', 2)))
+        self._openbci_channel_gain_edit.setValue(int(
+            self._settings.value(f'OpenBCIChannelGain{self._channel_number}', SETTINGS_OPENBCI_DEFAULT_GAIN)))
+
+    def save(self):
+        self._settings.setValue(f'OpenBCIChannelActivated{self._channel_number}',
+                                         self._openbci_channel_activated_check.checkState())
+        self._settings.setValue(f'OpenBCIChannelGain{self._channel_number}',
+                                         self._openbci_channel_gain_edit.value())
+
+
+class OpenBCIChannelsSettingsPage(QWidget):
+    def __init__(self, settings: Settings,parent=None):
+        super(OpenBCIChannelsSettingsPage, self).__init__(parent)
+        self.channel_list = list()
+
+    def load_settings(self):
+        layout = QVBoxLayout()
+        layout1 = QHBoxLayout()
+        layout2 = QHBoxLayout()
+        for i in range(SETTINGS_OPENBCI_DEFAULT_CHANNEL_NUMBER):
+            channel = OpenBCIChannelWidget(i)
+            channel.load_settings()
+            self.channel_list.append(channel)
+            if i < (SETTINGS_OPENBCI_DEFAULT_CHANNEL_NUMBER / 2):
+                layout1.addWidget(channel)
+            else:
+                layout2.addWidget(channel)
+
+        layout.addLayout(layout1)
+        layout.addLayout(layout2)
+        self.setLayout(layout)
+
+
+    def save(self):
+        for channel in self.channel_list:
+            channel.save()
+
+    @property
+    def title(self) -> str:
+        return 'Configuracion de los canales del OpenBCI'
