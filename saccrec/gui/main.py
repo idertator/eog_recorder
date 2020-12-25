138ad84d7a9d2776f38073a0f2f62eb8777825ea
Roberto Antonio Becerra García <rbecerra@topgroupexpress.com>
2020-12-26 00:36:24 +0100

    Reorganized GUI

--- saccrec/gui/main.py
+++ saccrec/gui/main.py
@@ -27,8 +27,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Related Widgets
         self._new_record_wizard: RecordSetupWizard = None
-        self._about_dialog = AboutDialog()
-        self._settings_dialog = SettingsDialog(self)
+        self._about_dialog = None
+        self._settings_dialog = None
 
         # Local Widgets
         self._signals_widget = SignalsWidget(self)
@@ -39,7 +39,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self._stimulus_player.started.connect(self._on_test_started)
         self._stimulus_player.stopped.connect(self._on_test_stopped)
         self._stimulus_player.finished.connect(self._on_test_finished)
-        self._stimulus_player.moved.connect(self._on_test_moved)
+        self._stimulus_player.moved.connect(self._on_stimulus_moved)
 
         # Setting up top level menus
         menubar = self.menuBar()
@@ -49,7 +49,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Setting up actions
         self._new_action = QtGui.QAction(QtGui.QIcon(':document.svg'), _('&New Recording'), self)
-        self._new_action.triggered.connect(self.on_new_test_wizard_clicked)
+        self._new_action.triggered.connect(self._on_new_action_clicked)
 
         self._exit_action = QtGui.QAction(QtGui.QIcon(':exit.svg'), _('&Exit'), self)
         self._exit_action.setShortcut('Ctrl+Q')
@@ -59,7 +59,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self._settings_action = QtGui.QAction(QtGui.QIcon(':settings.svg'), _('&Settings'), self)
         self._settings_action.setShortcut('Ctrl+P')
         self._settings_action.setStatusTip(_('Configure the application'))
-        self._settings_action.triggered.connect(self.open_settings_dialog)
+        self._settings_action.triggered.connect(self._on_settings_action_clicked)
 
         self._stop_action = QtGui.QAction(QtGui.QIcon(':stop-solid.svg'), _('&Stop'), self)
         self._stop_action.setShortcut('Ctrl+D')
@@ -67,7 +67,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self._stop_action.triggered.connect(self._on_stop_clicked)
 
         self._about_action = QtGui.QAction(QtGui.QIcon(':help.svg'), _('&About ...'), self)
-        self._about_action.triggered.connect(self.on_about_dialog_clicked)
+        self._about_action.triggered.connect(self._on_about_action_clicked)
 
         help_menu.addAction(self._about_action)
 
@@ -97,6 +97,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.show()
 
+    # ======================================
+    #         GUI State Management
+    # ======================================
+
     def _setup_gui_for_recording(self):
         self._new_action.setEnabled(False)
         self._exit_action.setEnabled(False)
@@ -113,30 +117,24 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self._stop_action.setEnabled(False)
 
-    def on_new_test_wizard_clicked(self):
+    # ======================================
+    #       Actions Events Handlers
+    # ======================================
+
+    def _on_new_action_clicked(self):
         if self._new_record_wizard is None:
             self._new_record_wizard = RecordSetupWizard(parent=self)
-            self._new_record_wizard.finished.connect(self.on_new_test_wizard_finished)
+            self._new_record_wizard.finished.connect(self._on_wizard_finished)
         self._new_record_wizard.show()
 
-    def on_new_test_wizard_finished(self, record_setup: dict):
-        self._setup_gui_for_recording()
-
-        self._new_record_wizard.finished.disconnect(self.on_new_test_wizard_finished)
-        self._new_record_wizard.destroy()
-        self._new_record_wizard = None
-
-        self._subject = record_setup['subject']
-        self._protocol = record_setup['protocol']
-        self._output_path = record_setup['output_path']
-        self._light_intensity = record_setup['light_intensity']
-
-        self.start()
-
-    def open_settings_dialog(self):
+    def _on_settings_action_clicked(self):
+        if self._settings_dialog is None:
+            self._settings_dialog = SettingsDialog(self)
         self._settings_dialog.open()
 
-    def on_about_dialog_clicked(self):
+    def _on_about_action_clicked(self):
+        if self._about_dialog is None:
+            self._about_dialog = AboutDialog(self)
         self._about_dialog.open()
 
     def _on_stop_clicked(self):
@@ -147,29 +145,32 @@ class MainWindow(QtWidgets.QMainWindow):
             QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.No
         )
         if answer == QtWidgets.QMessageBox.Ok:
-            self.stop()
+            self._setup_gui_for_non_recording()
 
-    def start(self):
+            self._current_test = None
+            self._is_running = False
+            self._stimulus_player.close()
+
+    # ======================================
+    #        App Flow Event Handlers
+    # ======================================
+
+    def _on_wizard_finished(self, record_setup: dict):
         self._setup_gui_for_recording()
 
+        self._new_record_wizard.finished.disconnect(self._on_wizard_finished)
+        self._new_record_wizard.destroy()
+        self._new_record_wizard = None
+
+        self._subject = record_setup['subject']
+        self._protocol = record_setup['protocol']
+        self._output_path = record_setup['output_path']
+        self._light_intensity = record_setup['light_intensity']
+
         self._current_test = 0
-        stimulus = self.protocol[0]
+        stimulus = self._protocol[0]
         self._stimulus_player.start(stimulus)
         self._is_running = True
-
-    def stop(self):
-        self._setup_gui_for_non_recording()
-
-        self._current_test = None
-        self._is_running = False
-        self._stimulus_player.close()
-
-    def finish(self):
-        self._setup_gui_for_non_recording()
-
-        self._current_test = 0
-        self._is_running = False
-        self._stimulus_player.close()
 
     def _on_test_started(self, timestamp):
         pass
@@ -185,7 +186,11 @@ class MainWindow(QtWidgets.QMainWindow):
             stimulus = self.protocol[self._current_test]
             self._stimulus_player.start(stimulus)
         else:
-            self.finish()
+            self._setup_gui_for_non_recording()
 
-    def _on_test_moved(self, value: int):
+            self._current_test = 0
+            self._is_running = False
+            self._stimulus_player.close()
+
+    def _on_stimulus_moved(self, value: int):
         pass
