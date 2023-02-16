0bc3af18e9d4f88e00ed151dbffa5225fc267aa9
Roberto Antonio Becerra García <idertator@gmail.com>
2023-02-16 16:14:04 +0100

    fix: Some bugs fixed

--- saccrec/core/enums.py
+++ saccrec/core/enums.py
@@ -1,15 +1,17 @@
-from enum import IntEnum, Enum
+from __future__ import annotations
+
+from enum import Enum, IntEnum
 
 
 class Language(Enum):
-    English = 'en'
-    Spanish = 'es'
+    English = "en"
+    Spanish = "es"
 
     @property
     def label(self) -> str:
         return {
-            Language.English: _('English'),
-            Language.Spanish: _('Spanish'),
+            Language.English: _("English"),
+            Language.Spanish: _("Spanish"),
         }[self]
 
 
@@ -22,26 +24,38 @@ class Gain(IntEnum):
     G12 = 5
     G24 = 6
 
+    @classmethod
+    def from_value(cls, value: int) -> Gain:
+        return {
+            1: Gain.G1,
+            2: Gain.G2,
+            4: Gain.G4,
+            6: Gain.G6,
+            8: Gain.G8,
+            12: Gain.G12,
+            24: Gain.G24,
+        }[value]
+
     @property
     def label(self) -> str:
         return {
-            Gain.G1: '1',
-            Gain.G2: '2',
-            Gain.G4: '4',
-            Gain.G6: '6',
-            Gain.G8: '8',
-            Gain.G12: '12',
-            Gain.G24: '24',
+            Gain.G1: "1",
+            Gain.G2: "2",
+            Gain.G4: "4",
+            Gain.G6: "6",
+            Gain.G8: "8",
+            Gain.G12: "12",
+            Gain.G24: "24",
         }[self]
 
     @property
     def settings(self) -> str:
         return {
-            Gain.G1: '0',
-            Gain.G2: '1',
-            Gain.G4: '2',
-            Gain.G6: '3',
-            Gain.G8: '4',
-            Gain.G12: '5',
-            Gain.G24: '6',
+            Gain.G1: "0",
+            Gain.G2: "1",
+            Gain.G4: "2",
+            Gain.G6: "3",
+            Gain.G8: "4",
+            Gain.G12: "5",
+            Gain.G24: "6",
         }[self]
